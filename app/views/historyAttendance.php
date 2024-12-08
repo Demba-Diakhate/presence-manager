f3e0be8c5d7b3011f2708723f85e43574ea6ef72
Demba-Diakhate <diakhatedemba@gmail.com>
2024-12-08 13:17:02 +0000

    Attendance history V1 final with menu hamberger

--- app/views/historyAttendance.php
+++ app/views/historyAttendance.php
@@ -41,14 +41,27 @@ $currentPage = basename($_SERVER['PHP_SELF']);
         <div>
             <img src="../../public/images/wommate.png" class="h-16" alt="Logo Wommate">
         </div>
-        <nav>
-            <ul class="flex justify-center items-center gap-5 text-white">
+        <div class="z-50 mt-32 -mr-8 md:z-0 md:mt-0 md:-mr-0 bg-inherit md:bg-inherit hidden md:block w-full md:w-auto md:order-1">
+             <ul class="flex flex-row justify-center items-center gap-5 text-white md:flex md:justify-center md:items-center md:gap-5 md:flex-row">
                 <li class="font-semibold <?= $currentPage === 'showStudents.php' ? ' text-white' : 'bg-fuchsia-800 border border-fuchsia-800 text-white' ?> px-4 py-2 rounded hover:bg-fuchsia-700 hover:ring-2 hover:ring-fuchsia-500"><a href="showStudents.php">Liste apprenants</a></li>
                 <li class="font-semibold <?= $currentPage === 'historyAttendance.php' ? 'bg-fuchsia-500 border border-fuchsia-500 text-white' : ' text-white' ?> px-4 py-2 rounded hover:ring-2 hover:ring-fuchsia-500"><a href="historyAttendance.php">Historique de présence</a></li>
                 <li class="flex items-center justify-center w-24 text-white bg-cyan-700 border border-cyan-700 hover:ring-2 hover:ring-cyan-500 font-medium rounded text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"><a href="addStudents.php">Ajouter</a></li>
             </ul>
-        </nav>
+        </div>
+        <button data-collapse-toggle="navbar-user" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
+        </button>
     </header>
+        <div class="z-50 -mt-1 pb-3 py-auto pt-3 md:z-0 md:pt-0 md:pl-0 md:pb-0 md:mt-0 md:-mr-0 bg-fuchsia-900 md:bg-inherit hidden md:hidden w-full md:w-auto md:order-1" id="navbar-user">
+            <ul class="flex flex-col justify-center items-center gap-5 text-white md:flex md:justify-center md:items-center md:gap-5 md:flex-row">
+                <li class="font-semibold w-5/12 flex items-center justify-center <?= $currentPage === 'showStudents.php' ? ' text-white' : 'bg-fuchsia-800 border border-fuchsia-800 text-white' ?> px-4 py-2 rounded hover:bg-fuchsia-700 hover:ring-2 hover:ring-fuchsia-500"><a href="showStudents.php">Liste apprenants</a></li>
+                <li class="font-semibold w-5/12 flex items-center justify-center <?= $currentPage === 'historyAttendance.php' ? 'bg-fuchsia-500 border border-fuchsia-500 text-white' : ' text-white' ?> px-4 py-2 rounded hover:ring-2 hover:ring-fuchsia-500"><a href="historyAttendance.php">Historique de présence</a></li>
+                <li class="flex items-center justify-center w-5/12 text-white bg-cyan-700 border border-cyan-700 hover:ring-2 hover:ring-cyan-500 font-medium rounded text-sm px-4 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"><a href="addStudents.php">Ajouter</a></li>
+            </ul>
+        </div>
     <section class="dark:bg-gray-900 p-3 sm:p-5">
         <div class="mx-auto px-4 lg:px-12">
             <div class="bg-white pb-5 w-12/12 dark:bg-gray-800 relative shadow-md sm:rounded-lg">
@@ -106,5 +119,6 @@ $currentPage = basename($_SERVER['PHP_SELF']);
                 </div>
             </div>
     </section>
+    <script src="../../node_modules/flowbite/dist/flowbite.min.js"></script>
 </body>
 </html>
--- app/views/showStudents.php
+++ app/views/showStudents.php
@@ -40,14 +40,30 @@ $currentPage = basename($_SERVER['PHP_SELF']);
         <div>
             <img src="../../public/images/wommate.png" class="h-16" alt="Logo Wommate">
         </div>
-        <nav>
-            <ul class="flex justify-center items-center gap-5 text-white">
-                <li class="font-semibold <?= $currentPage === 'showStudents.php' ? 'bg-fuchsia-500 border border-fuchsia-500 text-white' : ' text-white'?> px-4 py-2 rounded hover:ring-2 hover:ring-fuchsia-500"><a href="showStudents.php">Liste apprenants</a></li>
-                <li class="font-semibold <?= $currentPage === 'historyAttendance.php' ? ' text-white' : 'bg-fuchsia-800 border border-fuchsia-800 text-white'?> px-4 py-2 rounded hover:bg-fuchsia-700 hover:ring-2 hover:ring-fuchsia-500"><a href="historyAttendance.php">Historique de présence</a></li>
-                <li class="flex items-center justify-center text-white bg-cyan-700 border border-cyan-700 hover:ring-2 hover:ring-cyan-500 font-semibold rounded text-sm px-8 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"><a href="addStudents.php">Ajouter</a></li>
-            </ul>
-        </nav>
+            <div class="z-50 mt-32 -mr-8 md:z-0 md:mt-0 md:-mr-0 bg-inherit md:bg-inherit hidden md:block w-full md:w-auto md:order-1">
+            <!-- <nav> -->
+                <!-- <ul class="flex justify-center items-center gap-5 text-white"> -->
+                    <ul class="flex flex-row justify-center items-center gap-5 text-white md:flex md:justify-center md:items-center md:gap-5 md:flex-row">
+                        <li class="font-semibold <?= $currentPage === 'showStudents.php' ? 'bg-fuchsia-500 border border-fuchsia-500 text-white' : ' text-white'?> px-4 py-2 rounded hover:ring-2 hover:ring-fuchsia-500"><a href="showStudents.php">Liste apprenants</a></li>
+                        <li class="font-semibold <?= $currentPage === 'historyAttendance.php' ? ' text-white' : 'bg-fuchsia-800 border border-fuchsia-800 text-white'?> px-4 py-2 rounded hover:bg-fuchsia-700 hover:ring-2 hover:ring-fuchsia-500"><a href="historyAttendance.php">Historique de présence</a></li>
+                        <li class="flex items-center justify-center text-white bg-cyan-700 border border-cyan-700 hover:ring-2 hover:ring-cyan-500 font-semibold rounded text-sm px-8 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-fuchsia-500"><a href="addStudents.php">Ajouter</a></li>
+                    </ul>
+                <!-- </nav> -->
+            </div>
+            <button data-collapse-toggle="navbar-user" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
+            </button>
     </header>
+            <div class="z-50 -mt-1 pb-3 py-auto pt-3 md:z-0 md:pt-0 md:pl-0 md:pb-0 md:mt-0 md:-mr-0 bg-fuchsia-900 md:bg-inherit hidden md:hidden w-full md:w-auto md:order-1" id="navbar-user">
+                    <ul class="flex flex-col justify-center items-center gap-5 text-white md:flex md:justify-center md:items-center md:gap-5 md:flex-row">
+                        <li class="font-semibold w-5/12 flex items-center justify-center <?= $currentPage === 'showStudents.php' ? 'bg-fuchsia-500 border border-fuchsia-500 text-white' : ' text-white'?> px-4 py-2 rounded hover:ring-2 hover:ring-fuchsia-500"><a href="showStudents.php">Liste apprenants</a></li>
+                        <li class="font-semibold w-5/12 flex items-center justify-center <?= $currentPage === 'historyAttendance.php' ? ' text-white' : 'bg-fuchsia-800 border border-fuchsia-800 text-white'?> px-4 py-2 rounded hover:bg-fuchsia-700 hover:ring-2 hover:ring-fuchsia-500"><a href="historyAttendance.php">Historique de présence</a></li>
+                        <li class="flex items-center justify-center w-5/12 text-white bg-cyan-700 border border-cyan-700 hover:ring-2 hover:ring-cyan-500 font-semibold rounded text-sm px-8 py-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-fuchsia-500"><a href="addStudents.php">Ajouter</a></li>
+                    </ul>
+            </div>
     <section class="dark:bg-gray-900 p-3 sm:p-5">
         <div class="mx-auto px-4 lg:px-12">
             <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg">
@@ -156,32 +172,6 @@ $currentPage = basename($_SERVER['PHP_SELF']);
                 </div>
                 
             </div>
-            
-            <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 left-0 right-0 z-50 mx-[550px] my-60 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                <div class="relative p-4 w-full max-w-md max-h-full">
-                    <div class="relative bg-stone-100 rounded-lg shadow dark:bg-gray-700">
-                        <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
-                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                            </svg>
-                            <span class="sr-only">Close modal</span>
-                        </button>
-                        <div class="p-4 md:p-5 text-center">
-                            <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-                            </svg>
-                            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
-                            <button data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
-                                Yes, I'm sure
-                            </button>
-                            <button data-modal-hide="popup-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-
         </section>
     <script src="../../node_modules/flowbite/dist/flowbite.min.js"></script>
 </body>
